85c97cca9af7ee9ea5ea4c21dd9941b73309a938
Daein Park <bysnupy@hotmail.com>
2019-03-10 12:37:18 +0900

    Replace kubectl with env variable and add default cluster env variable
    Already defined "CO_CMD=kubectl" but not used it in the script and Makefile, so I replace hard coded "kubectl" with the env variable, and add "CO_CLUSTER" env variable for enhancement to make change easily cluster name on various user system.

--- examples/envs.sh
+++ examples/envs.sh
@@ -8,6 +8,7 @@ export CO_IMAGE_PREFIX=crunchydata
 export CO_BASEOS=centos7
 export CO_VERSION=3.5.1
 export CO_IMAGE_TAG=$CO_BASEOS-$CO_VERSION
+export CO_CLUSTER=kubernetes
 
 # for the pgo CLI auth
 export PGO_CA_CERT=$COROOT/conf/postgres-operator/server.crt
@@ -16,17 +17,17 @@ export PGO_CLIENT_KEY=$COROOT/conf/postgres-operator/server.key
 
 # useful functions
 setip() {
-	export CO_APISERVER_URL=https://`kubectl -n "$CO_NAMESPACE" get service postgres-operator -o=jsonpath="{.spec.clusterIP}"`:8443
+	export CO_APISERVER_URL=https://`$CO_CMD -n "$CO_NAMESPACE" get service postgres-operator -o=jsonpath="{.spec.clusterIP}"`:8443
 }
 
 alog() {
-	kubectl  -n "$CO_NAMESPACE" logs `kubectl  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c apiserver
+	$CO_CMD  -n "$CO_NAMESPACE" logs `$CO_CMD  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c apiserver
 }
 
 olog() {
-	kubectl  -n "$CO_NAMESPACE" logs `kubectl  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c operator
+	$CO_CMD  -n "$CO_NAMESPACE" logs `$CO_CMD  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c operator
 }
 
 slog() {
-	kubectl  -n "$CO_NAMESPACE" logs `kubectl  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c scheduler
+	$CO_CMD  -n "$CO_NAMESPACE" logs `$CO_CMD  -n "$CO_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c scheduler
 }
